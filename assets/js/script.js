4b4c94383d2ccade6b16a920013455abf220a20c
rinalds98 <rinalds98@gmail.com>
2022-10-09 22:00:46 +0000

    Add quiz array

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+let countries = [
+    {
+        image : img.src="assets/images/england.jpg",
+        country1 : "England",
+        country2 : "France",
+        answer : "England",
+    },
+    {
+        image : img.src="assets/images/peru.jpg",
+        country1 : "Peru",
+        country2 : "Argentina",
+        answer : "Peru",
+    },
+    {
+        image : img.src="assets/images/japan.jpg",
+        country1 : "Japan",
+        country2 : "China",
+        answer : "Japan",
+    },
+    {
+        image : img.src="assets/images/italy.jpg",
+        country1 : "Italy",
+        country2 : "Germany",
+        answer : "Italy",
+    },
+    {
+        image : img.src="assets/images/iceland.jpg",
+        country1 : "Iceland",
+        country2 : "Norway",
+        answer : "Iceland",
+    },
+
+
+
+]

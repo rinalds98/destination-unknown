33bb9b3d27874b66820b253bc73b9b54041ee8e8
rinalds98 <rinalds98@gmail.com>
2022-10-12 19:42:52 +0000

    Add images and text from array to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,31 +18,31 @@ document.addEventListener("DOMContentLoaded", function(){
 
 let countries = [
     {
-        image : img.src="assets/images/england.jpg",
+        image : "assets/images/england.jpg",
         country1 : "England",
         country2 : "France",
         answer : "England",
     },
     {
-        image : img.src="assets/images/peru.jpg",
+        image : "assets/images/peru.jpg",
         country1 : "Peru",
         country2 : "Argentina",
         answer : "Peru",
     },
     {
-        image : img.src="assets/images/japan.jpg",
+        image : "assets/images/japan.jpg",
         country1 : "Japan",
         country2 : "China",
         answer : "Japan",
     },
     {
-        image : img.src="assets/images/italy.jpg",
+        image : "assets/images/italy.jpg",
         country1 : "Italy",
         country2 : "Germany",
         answer : "Italy",
     },
     {
-        image : img.src="assets/images/iceland.jpg",
+        image : "assets/images/iceland.jpg",
         country1 : "Iceland",
         country2 : "Norway",
         answer : "Iceland",
@@ -51,6 +51,22 @@ let countries = [
 
 
 ]
+//This adds an image and text to buttons from the array
+let image = document.getElementsByClassName("image");
+image[0].setAttribute("src", countries[0].image);
+
+let button = document.getElementsByClassName("btn");
+button[0].innerHTML = countries[0].country1;
+button[1].innerHTML = countries[0].country2;
+
+
+
+//imageLocation = document.getElementsByClassName("imageload");
+//let img = document.createElement("img");
+//img.setAttribute("src", countries[0].image);
+//img.setAttribute("class", "image");
+//imageLocation[0].appendChild(img);
+
 
 function runQuiz(){
 

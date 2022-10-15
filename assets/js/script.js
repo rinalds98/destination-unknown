c8321dac2b47ba41fc26c5ce44eb57667dc39998
rinalds98 <rinalds98@gmail.com>
2022-10-15 11:29:51 +0000

    Add start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,4 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
-
-function main() {
-    return 'Hello, World!';
-  }
-  
-  main();
-  // Global variable for looping through different quiz questions
+ // Global variable for looping through different quiz questions
   
   let i = 0;
   // Global array of quiz questions
@@ -59,29 +43,41 @@ function main() {
   
       for (let button of buttons){
           button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "start"){
+                startGame();
+          }else{
                   let countryType = this.getAttribute("data-type");
                   checkAnswer(countryType);
+          }
           });
       }
       loader();
   });
-  
+
+function startGame(){
+   let stuff = document.getElementsByClassName("game-area")[0];
+   let btn = document.getElementsByClassName("btn")[0];
+   let score = document.getElementsByClassName("score-area")[0];
+   btn.style.display = "none";
+   stuff.style.display = "block";
+   score.style.display = "inline-block";
+}
   // This adds an image and text to buttons from the array
   
-      function loader(){
-          let image = document.getElementsByClassName("image");
-          image[0].setAttribute("src", countries[i].image);
-      
-          let button = document.getElementsByClassName("btn");
-          button[0].innerHTML = countries[i].country1;
-          button[0].dataset.type = countries[i].country1;
-          button[1].innerHTML = countries[i].country2;
-          button[1].dataset.type = countries[i].country2;
-      }
+function loader(){
+   let image = document.getElementsByClassName("image");
+   image[0].setAttribute("src", countries[i].image);
+   let button = document.getElementsByClassName("btn");
+   button[1].innerHTML = countries[i].country1;
+   button[1].dataset.type = countries[i].country1;
+   button[2].innerHTML = countries[i].country2;
+   button[2].dataset.type = countries[i].country2;
+}
   
-  // Checks the answer the user selected
+  // Checks the answer the user selected and modifies CSS with a timer
   
   function checkAnswer(countryType){
+    console.log(i);
     let background = document.getElementsByClassName("imageload")[0];
     let org = background.style.backgroundColor;
     if (countryType === countries[i].answer){
@@ -106,11 +102,10 @@ function main() {
       
   }
   
-  // Increments the score if the user selected the correct answer
+  // Increments the score if the user selected the incorrect answer
   
   function incrementWrongAnswer(){
       let oldScore = parseInt(document.getElementById("incorrect").innerText);
       document.getElementById("incorrect").innerText = ++oldScore;
   }
   
-
